e7fa93d08759d8dc1c6296adfd8682a1b75ad067
Lasha <lashakakashvili114@gmail.com>
2023-01-12 22:39:59 +0400

    Minor

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).on("keypress", function (e) {
-  if (e.which == 	32) {
+  if (e.which == 32) {
     $("#TESTWORD").html("");
     $("#DEFINITON").html("");
     $("#Push_here").empty();
@@ -24,17 +24,19 @@ $(document).on("keypress", function (e) {
                   )
                 );
             } else {
-                $.ajax({
-                    method: "GET",
-                    url: "https://api.api-ninjas.com/v1/randomword?type",
-                    headers: { "X-Api-Key": "VI6ivRV3/4F56OSmro3Dow==iDcEphCADVrguHal" },
-                    contentType: "application/json",
-                    success: function (result) {      },
+              $.ajax({
+                method: "GET",
+                url: "https://api.api-ninjas.com/v1/randomword?type",
+                headers: {
+                  "X-Api-Key": "VI6ivRV3/4F56OSmro3Dow==iDcEphCADVrguHal",
+                },
+                contentType: "application/json",
+                success: function (result) {},
 
-                    error: function ajaxError(jqXHR) {
-                      console.error("Error: ", jqXHR.responseText);
-                    },
-                  });
+                error: function ajaxError(jqXHR) {
+                  console.error("Error: ", jqXHR.responseText);
+                },
+              });
             }
           }
         );
@@ -50,9 +52,10 @@ $(document).on("keypress", function (e) {
         colorEm();
         function colorEm() {
           var $div = $("#Push_here #Child");
-          var $innerDiv = $("#Push_here #Inner")
+          var $innerDiv = $("#Push_here #Inner");
           var start = Math.floor(Math.random() * $div.length - 4);
-          var end = Math.floor(Math.random() * ($div.length - start)) + start + 2;
+          var end =
+            Math.floor(Math.random() * ($div.length - start)) + start + 2;
           if (end === $div.length) {
             end = undefined;
           }
@@ -63,88 +66,78 @@ $(document).on("keypress", function (e) {
           } else {
             $div.slice(start).css("opacity", "0.1");
             $div.slice(start, end).addClass("Missing");
-
           }
           //console.log($div[start])
         }
         var $div = $("#Push_here span");
-        var $missing = $(".Missing")
+        var $missing = $(".Missing");
         var start = Math.floor(Math.random() * $div.length - 4);
         var end = Math.floor(Math.random() * ($div.length - start)) + start + 2;
 
+        setTimeout(function () {
+          var MissingWord = $("#Push_here .Missing")
+            .map(function () {
+              return this.innerHTML;
+            })
+            .get();
 
-
-
-      setTimeout(
-        function() 
-        {
-          var MissingWord = $("#Push_here .Missing").map(function() {
-            return this.innerHTML;
-        }).get();
-        
-        let wordsArray = [];
-        wordsArray.push(MissingWord)
-
-        for (let i = 0; i < MissingWord.length; i++) {
-          console.log(MissingWord[i])
-        }
-
-        
-        console.log('============')
-   
-
-      //   $("#GetInput").keyup(function(event) {
-      //     if (event.keyCode === 13) {
-      //       let val = $("#GetInput").val();
-      //       for (let i = 0; i < MissingWord.length; i++) {
-      //         console.log(MissingWord[i])
-      //         if(val == MissingWord[i] ){
-      //           console.log("MATCH FOUND")
-      //         }          
-      //         else{
-      //           console.log("MATCH NOT FOUND")
-      //         }
-      //       }
-      //       console.log(`Player Input Value Is ${val}`)
-      //       $("#GetInput").val("");
-      //     }
-      // });
-
-
-      //! Better Version
-      $("#GetInput").keyup(function(event) {
-        if (event.keyCode === 13) {
+          let wordsArray = [];
+          wordsArray.push(MissingWord);
 
           for (let i = 0; i < MissingWord.length; i++) {
-            console.log(MissingWord[i])
+            console.log(MissingWord[i]);
           }
-          let val = $("#GetInput").val();
-          let $missing = $(".Missing");
-          let $innerDiv = $("#Push_here #Inner")
-          $missing.each(function() {
-            if ($(this).text() === val) {
-              $(this).css("background-color", "green");
-              $($innerDiv).css("opacity", "1");
-              $(this).css("opacity", "1");
-              $(this).removeClass("Missing");
-              $("#GetInput").val("");
+
+          console.log("============");
+
+          //   $("#GetInput").keyup(function(event) {
+          //     if (event.keyCode === 13) {
+          //       let val = $("#GetInput").val();
+          //       for (let i = 0; i < MissingWord.length; i++) {
+          //         console.log(MissingWord[i])
+          //         if(val == MissingWord[i] ){
+          //           console.log("MATCH FOUND")
+          //         }
+          //         else{
+          //           console.log("MATCH NOT FOUND")
+          //         }
+          //       }
+          //       console.log(`Player Input Value Is ${val}`)
+          //       $("#GetInput").val("");
+          //     }
+          // });
+
+          //! Better Version
+          $("#GetInput").keyup(function (event) {
+            if (event.keyCode === 13) {
+              for (let i = 0; i < MissingWord.length; i++) {
+                console.log(MissingWord[i]);
+              }
+              let val = $("#GetInput").val();
+              let $missing = $(".Missing");
+              let $innerDiv = $("#Push_here #Inner");
+              $missing.each(function () {
+                if ($(this).text() === val) {
+                  $(this).css("background-color", "green");
+                  $($innerDiv).css("opacity", "1");
+                  $(this).css("opacity", "1");
+                  $(this).removeClass("Missing");
+                  $("#GetInput").val("");
+                }
+              });
+
+              if ($missing.length === 0) {
+                alert("You Won! Press Space for new word");
+              }
             }
           });
-      
-          if ($missing.length === 0) {
-            alert("All missing elements have been filled in correctly!");
-          }
-        }
-      });
 
-      //! ends Here
+          //! ends Here
           //do something special
         }, 3000);
-        
+
         colorEm();
       },
-      
-
 
       error: function ajaxError(jqXHR) {
         console.error("Error: ", jqXHR.responseText);
@@ -156,4 +149,4 @@ $(document).on("keypress", function (e) {
   }
 });
 
-//* API IS DONE 
+//* API IS DONE
